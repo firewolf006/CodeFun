33202ee169e2b20a3a9faafb4b68a5e946704943
firewolf006 <lesters006@gmail.com>
2014-09-16 00:16:28 -0400

    0

--- CodeEvalFun/Program.cs
+++ CodeEvalFun/Program.cs
@@ -10,13 +10,21 @@ class Program
         using (StreamReader reader = File.OpenText(args[0]))
             while (!reader.EndOfStream)
             {
+                string[] temp;
+                mewtwo = 0;
                 string line = reader.ReadLine();
                 if (null == line)
                     continue;
-                mewtwo += int.Parse(line);
+                temp = line.Split(' ');
+                for (int bagel = 0; bagel < temp.Length; bagel++)
+                {
+                    if (temp[bagel].Length > temp[mewtwo].Length)
+                    {
+                        mewtwo = bagel;
+                    }
+                }
                 // do something with line
-                Console.WriteLine(mewtwo);
+                Console.WriteLine(temp[mewtwo]);
             }
-        
     }
 }
