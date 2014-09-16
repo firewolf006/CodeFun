ec18333df56ba50299375f69de4a2a143aad0fbc
firewolf006 <lesters006@gmail.com>
2014-09-15 23:45:40 -0400

    01

---
+++ Program - Copy.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        using (StreamReader reader = File.OpenText(args[0]))
+            while (!reader.EndOfStream)
+            {
+                string line = reader.ReadLine();
+                if (null == line)
+                    continue;
+                // do something with line
+                int space1 = 0;
+                int space2 = 0;
+                for (int m = line.Length - 1; m >= 0; m--)
+                {
+                    if (line[m] == ' ')
+                    {
+                        space1 = m - 1;
+                        m = -1;
+                    }
+
+                }
+                for (int m = space1; m >= 0; m--)
+                {
+                    if (line[m] == ' ')
+                    {
+                        space2 = m + 1;
+                        m = -1;
+                    }
+                }
+
+                Console.WriteLine(line.Substring(space2,space1-space2+1));
+            }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int mewtwo = 0;
+        using (StreamReader reader = File.OpenText(args[0]))
+            while (!reader.EndOfStream)
+            {
+                string line = reader.ReadLine();
+                if (null == line)
+                    continue;
+                mewtwo += int.Parse(line);
+                // do something with line
+                Console.WriteLine(mewtwo);
+            }
+        
+    }
+}
