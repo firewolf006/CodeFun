14d672598ff6afd7ed886411359f9bc9cfe0cbeb
firewolf006 <lesters006@gmail.com>
2014-09-17 08:03:35 -0400

    beatiful strings

--- CodeEvalFun/Program.cs
+++ CodeEvalFun/Program.cs
@@ -6,25 +6,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        int mewtwo = 0;
+        string codex = "defghijklmnopqrstuvwxyzabc";
         using (StreamReader reader = File.OpenText(args[0]))
             while (!reader.EndOfStream)
             {
-                string[] temp;
-                mewtwo = 0;
+                //string[] temp;
+                int mewtwo = 0;
                 string line = reader.ReadLine();
                 if (null == line)
                     continue;
-                temp = line.Split(' ');
-                for (int bagel = 0; bagel < temp.Length; bagel++)
+                string m = "";
+                for (int bagel = 0; bagel < line.Length; bagel++)
                 {
-                    if (temp[bagel].Length > temp[mewtwo].Length)
-                    {
-                        mewtwo = bagel;
-                    }
+                    if (Convert.ToChar(line.Substring(bagel, 1).ToLower()) >= (int)'a' && Convert.ToChar(line.Substring(bagel, 1).ToLower()) <= (int)'z'){
+                    mewtwo += Convert.ToInt32(codex.IndexOf(line.Substring(bagel, 1).ToLower().ToString())+1);
+                    m += line.Substring(bagel, 1).ToLower().ToString();}
                 }
                 // do something with line
-                Console.WriteLine(temp[mewtwo]);
+                Console.WriteLine(mewtwo);
             }
     }
 }
